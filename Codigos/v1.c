0eb0ed7803f7e783d88e24d59c890d2b12c720c5
Vxbd <victorxesus.barreiro@rai.usc.es>
2020-05-03 12:45:25 +0200

    PreTempos

--- Codigos/v1.c
+++ Codigos/v1.c
@@ -1,14 +1,20 @@
+/*
+Autores: Barreiro Dominguez, Víctor Xesús e Castro Castreje, Helena
+Titulo: 
 
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cmath.h>
+
 
 typedef struct quaternion{
 	double q[4];
 }quaternion_t;
 
-#define N 1
+#define N 5	
 
 void iniCuatR(quaternion_t* cuat);
 void iniCuat(quaternion_t* cuat);
@@ -28,6 +34,17 @@ int main(int argc, char const *argv[])
 	iniCuatR(a);
 	iniCuatR(b);
 
+	/*
+	a[0].q[0] = 1;
+	a[0].q[1] = 2;
+	a[0].q[2] = 3;
+	a[0].q[3] = 4;
+
+	b[0].q[0] = 4;
+	b[0].q[1] = 3;
+	b[0].q[2] = 2;
+	b[0].q[3] = 1;
+	*/
 
 	//Imprimimos los vectores de cuaterniones
 	printf("Vector A\n");
@@ -46,7 +63,7 @@ int main(int argc, char const *argv[])
 	{
 		c[i].q[0] = a[i].q[0]*b[i].q[0] - a[i].q[1]*b[i].q[1] - a[i].q[2]*b[i].q[2] - a[i].q[3]*b[i].q[3];
 		c[i].q[1] = a[i].q[0]*b[i].q[1] + a[i].q[1]*b[i].q[0] + a[i].q[2]*b[i].q[3] - a[i].q[3]*b[i].q[2];
-		c[i].q[2] = a[i].q[0]*b[i].q[2] - a[i].q[1]*b[i].q[3] + a[i].q[2]*b[i].q[1] + a[i].q[3]*b[i].q[1];
+		c[i].q[2] = a[i].q[0]*b[i].q[2] - a[i].q[1]*b[i].q[3] + a[i].q[2]*b[i].q[0] + a[i].q[3]*b[i].q[1];
 		c[i].q[3] = a[i].q[0]*b[i].q[3] + a[i].q[1]*b[i].q[2] - a[i].q[2]*b[i].q[1] + a[i].q[3]*b[i].q[0];
 	}
 
@@ -60,11 +77,13 @@ int main(int argc, char const *argv[])
 
 	for (int i = 0; i < N; ++i)
 	{
+		printf("Itersions :%d\n", i);
 		dp.q[0] += c[i].q[0]*c[i].q[0] - c[i].q[1]*c[i].q[1] - c[i].q[2]*c[i].q[2] - c[i].q[3]*c[i].q[3];
-		dp.q[1] += c[i].q[0]*c[i].q[1] + c[i].q[1]*b[i].q[0] + c[i].q[2]*c[i].q[3] - c[i].q[3]*c[i].q[2];
-		dp.q[2] += c[i].q[0]*c[i].q[2] - c[i].q[1]*b[i].q[3] + c[i].q[2]*c[i].q[1] + c[i].q[3]*c[i].q[1];
-		dp.q[3] += c[i].q[0]*c[i].q[3] + c[i].q[1]*b[i].q[2] - c[i].q[2]*c[i].q[1] + (c[i].q[3])*(c[i].q[0]);
+		dp.q[1] += c[i].q[0]*c[i].q[1] + c[i].q[1]*c[i].q[0] + c[i].q[2]*c[i].q[3] - c[i].q[3]*c[i].q[2];
+		dp.q[2] += c[i].q[0]*c[i].q[2] - c[i].q[1]*c[i].q[3] + c[i].q[2]*c[i].q[0] + c[i].q[3]*c[i].q[1];
+		dp.q[3] += c[i].q[0]*c[i].q[3] + c[i].q[1]*c[i].q[2] - c[i].q[2]*c[i].q[1] + c[i].q[3]*c[i].q[0];
 	}
+
 
 	printf("Cuaterninon\n");
 	imprimirCuat(dp);
@@ -79,8 +98,7 @@ void iniCuatR(quaternion_t* cuat)
 	{
 		for (int i = 0; i < 4; ++i)
 		{
-			printf("i-> %d\nj-> %d\n", i,j);
-			cuat[j].q[i] = rand()/rand();
+			cuat[j].q[i] = (rand() % 1000000) * 0.00001;
 		}
 	}
 }
@@ -95,13 +113,13 @@ void iniCuat(quaternion_t* cuat)
 
 void imprimirCuat(quaternion_t cuat)
 {
-	printf(" Cuat: (%lf, %lf, %lf, %lf)\n", cuat.q[0], cuat.q[1], cuat.q[2], cuat.q[3]);
+	printf(" Cuat: (%.3lf, %.3lf, %.3lf, %.3lf)\n", cuat.q[0], cuat.q[1], cuat.q[2], cuat.q[3]);
 }
 
 
-/*void prod(double* a, double* b, double* c)
+/*
+void prod(double* a, double* b, double* c)
 {
-
 	for (int i = 0; i < N; ++i)
 	{
 		c[i].q[0] = a[i].q[0]*b[i].q[0] - a[i].q[1]*b[i].q[1] - a[i].q[2]*b[i].q[2] - a[i].q[3]*b[i].q[3];
@@ -109,12 +127,10 @@ void imprimirCuat(quaternion_t cuat)
 		c[i].q[2] = a[i].q[0]*b[i].q[2] - a[i].q[1]*b[i].q[3] + a[i].q[2]*b[i].q[1] + a[i].q[3]*b[i].q[1];
 		c[i].q[3] = a[i].q[0]*b[i].q[3] + a[i].q[1]*b[i].q[2] - a[i].q[2]*b[i].q[1] + a[i].q[3]*b[i].q[0];
 	}
-	
 }
 
 void opera(double* c, double* dp)
 {
-
 	for (int i = 0; i < N; ++i)
 	{
 		dp.q[0] += c[i].q[0]*c[i].q[0] - c[i].q[1]*c[i].q[1] - c[i].q[2]*c[i].q[2] - c[i].q[3]*c[i].q[3];
@@ -122,5 +138,5 @@ void opera(double* c, double* dp)
 		dp.q[2] += c[i].q[0]*c[i].q[2] - c[i].q[1]*b[i].q[3] + c[i].q[2]*c[i].q[1] + c[i].q[3]*c[i].q[1];
 		dp.q[3] += c[i].q[0]*c[i].q[3] + c[i].q[1]*b[i].q[2] - c[i].q[2]*c[i].q[1] + c[i].q[3]*c[i].q[0];
 	}
-	
-}*/
+}
+*/
